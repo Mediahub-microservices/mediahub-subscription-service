d478770f69304c42bfc5d75652703a0b7943f4e6
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2026-03-08 22:00:07 +0000

    l'implémentation de la classe CreateSubscriptionRequest

--- src/main/java/com/mediahub/subscription_service/dto/request/CreateSubscriptionRequest.java
+++ src/main/java/com/mediahub/subscription_service/dto/request/CreateSubscriptionRequest.java
@@ -1,4 +1,31 @@
 package com.mediahub.subscription_service.dto.request;
 
+import com.mediahub.subscription_service.domain.Plan;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateSubscriptionRequest {
+
+    @NotNull
+    private UUID userId;
+
+    @NotNull
+    private Plan planType;
+
+    @NotNull
+    private BigDecimal price;
+
+    @NotNull
+    private LocalDateTime startDate;
+
+    @NotNull
+    private LocalDateTime endDate;
 }
